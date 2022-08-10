cc39e26ed6b5cd0c29a4553577b650fdd69f6752
George Anya <georgeanya4real@gmail.com>
2022-08-10 20:25:06 +0100

    commit

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex  cursor-pointer items-center'>
+      <p className="text-md font-mono  font-semibold uppercase tracking-wider text-slate-500">My Github Finder</p>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ components/searchbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const searchBar = () => {
+  return (
+    <div className='align-items mx-auto mt-4 flex max-w-md justify-between space-x-2 rounded-lg bg-gray-200 p-2 pb-2 transition duration-300 ease-in dark:bg-[#2b365e] md:max-w-2xl"'>
+      
+    </div>
+  )
+}
+
+export default searchBar
